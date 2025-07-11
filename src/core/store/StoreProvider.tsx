bc844bb54d788f92f2f8fcba53a6fa986e5d9dca
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-07-11 15:21:46 +0900

    코드 정리

--- src/core/store/StoreProvider.tsx
+++ src/core/store/StoreProvider.tsx
@@ -2,6 +2,7 @@
 
 import { Provider } from 'react-redux'
 import store from './reduxStore'
+import React from "react";
 
 export function StoreProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>
